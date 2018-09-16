8c192f2c66ef760cdc5095941065c35dbe169634
sqzaman <sqzaman@gmail.com>
2018-09-15 22:26:59 -0500

    initial commit

---
+++ backend/iBlogCoreService/src/main/java/iblog/blog/IBlogCoreServiceApplication.java
@@ -0,0 +1,12 @@
+package iblog.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IBlogCoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IBlogCoreServiceApplication.class, args);
+	}
+}
---
+++ backend/iBlogSearchService/src/test/java/iblog/blog/IBlogSearchServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package iblog.blog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IBlogSearchServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
